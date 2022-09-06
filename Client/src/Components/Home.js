cd3a7d8c4ca078bf571063f494620e33f0a7598c
kadampratik.engg <kadampratik.engg@gmail.com>
2022-09-06 17:59:36 +0530

    20

--- Client/src/Components/Home.js
+++ Client/src/Components/Home.js
@@ -1,87 +1,34 @@
 import React from "react";
-import Button from "@material-ui/core/Button";
-import MobileStepper from "@material-ui/core/MobileStepper";
-import Paper from "@material-ui/core/Paper";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
-import Typography from "@material-ui/core/Typography";
-import { useTheme } from "@material-ui/core/styles";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-
-const MyCollection = [
-{
-	imgPath:
-"https://wonderfulengineering.com/wp-content/uploads/2014/10/wallpaper-photos-31.jpg",
-},
-{
-	imgPath:
-"https://media.geeksforgeeks.org/wp-content/uploads/20210208000009/2.png",
-},
-{
-	imgPath:
-"https://media.geeksforgeeks.org/wp-content/uploads/20210208000008/3.png",
-},
-{
-	imgPath:
-"https://wonderfulengineering.com/wp-content/uploads/2014/10/wallpaper-photos-31.jpg",
-},
-{
-	imgPath:
-"https://media.geeksforgeeks.org/wp-content/uploads/20210208000009/2.png",
-},
-{
-	imgPath:
-"https://media.geeksforgeeks.org/wp-content/uploads/20210208000008/3.png",
-},
-];
-
-const App = () => {
-const CollectionSize = MyCollection.length;
-const theme = useTheme();
-const [index, setActiveStep] = React.useState(0);
-
-const goToNextPicture = () => {
-	setActiveStep((prevActiveStep) => prevActiveStep + 1);
-};
-
+const Home = () => {
 return (
-	<div>
-	
-	<div>
-		<Paper>
-		<Typography>{MyCollection[index].label}</Typography>
-		</Paper>
-		<img
-		src={MyCollection[index].imgPath}
-		style={{
-			height: 600,
-			width: "100%",
-      
-		}}
-		alt={MyCollection[index].label}
-		/>
-		<MobileStepper
-		variant="text"
-		position="static"
-		index={index}
-		steps={CollectionSize}
-		nextButton={
-			<Button
-			size="small"
-			onClick={goToNextPicture}
-			disabled={index === CollectionSize - 1}
-			>
-			Next
-			{theme.direction !== "rtl" ? (
-				<KeyboardArrowRight />
-			) : (
-				<KeyboardArrowLeft />
-			)}
-			</Button>
-		}
-		/>
-	</div>
-	</div>
+<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+  <ol className="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div className="carousel-inner">
+    <div className="carousel-item active">
+      <img className="d-block w-10" src="https://wonderfulengineering.com/wp-content/uploads/2014/10/wallpaper-photos-31.jpg" alt="First slide" style={  {width: '100%', height:'80vh'}}/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-10" src="https://wallpaperaccess.com/full/51382.jpg" alt="Second slide" style={  {width: '100%', height:'80vh'}}/>
+    </div>
+    <div className="carousel-item">
+      <img className="d-block w-10" src="https://image.shutterstock.com/image-photo/3d-room-wallpaper-flower-beautiful-260nw-2138393387.jpg" alt="Third slide" style={  {width: '100%', height:'80vh'}}/>
+    </div>
+  </div>
+  <a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span className="sr-only">Previous</span>
+  </a>
+  <a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+    <span className="sr-only">Next</span>
+  </a>
+</div>
+
 );
 };
 
-export default App;
+export default Home;
