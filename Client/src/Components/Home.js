01d28f4c46ad8f9313918e8d48c31d42ff130f4b
kadampratik.engg <kadampratik.engg@gmail.com>
2022-09-07 12:18:17 +0530

    06sep2022

--- Client/src/Components/Home.js
+++ Client/src/Components/Home.js
@@ -1,16 +1,41 @@
 import React from 'react'
 
-function Home() {
-  return (
-    <div>
 
-      <img alt="Qries" src="https://wonderfulengineering.com/wp-content/uploads/2014/10/wallpaper-photos-31.jpg" width="100%" height="600" />
+const Home = () =>{
+		return (
+			<div>
 
+				{/* <img alt="Qries" src="https://wonderfulengineering.com/wp-content/uploads/2014/10/wallpaper-photos-31.jpg" width="100%" height="600" /> */}
 
-
-    </div>
-  )
-}
+				<div id="carouselExampleIndicators" className="carousel slide" data-ride="carousel">
+					<ol className="carousel-indicators">
+						<li data-target="#carouselExampleIndicators" data-slide-to="0" className="active"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+						<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+					</ol>
+					<div className="carousel-inner">
+						<div className="carousel-item active">
+							<img className="d-block w-10" src="https://wonderfulengineering.com/wp-content/uploads/2014/10/wallpaper-photos-31.jpg" alt="First slide" style={{ width: '100%', height: '100vh' }} />
+						</div>
+						<div className="carousel-item">
+							<img className="d-block w-10" src="https://wallpaperaccess.com/full/51382.jpg" alt="Second slide" style={{ width: '100%', height: '100vh' }} />
+						</div>
+						<div className="carousel-item">
+							<img className="d-block w-10" src="https://image.shutterstock.com/image-photo/3d-room-wallpaper-flower-beautiful-260nw-2138393387.jpg" alt="Third slide" style={{ width: '100%', height: '100vh' }} />
+						</div>
+					</div>
+					<a className="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+						<span className="carousel-control-prev-icon" aria-hidden="true"></span>
+						<span className="sr-only">Previous</span>
+					</a>
+					<a className="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+						<span className="carousel-control-next-icon" aria-hidden="true"></span>
+						<span className="sr-only">Next</span>
+					</a>
+				</div>
+			</div>
+		)
+	}
 
 export default Home
 
