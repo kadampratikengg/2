4877fddef69ee8b994c397c458ba212e25849154
unknown <Administrator@ANGULARMINDS.COM>
2023-04-04 17:31:03 +0530

    04042023

--- Client/src/Components/Contact.js
+++ Client/src/Components/Contact.js
@@ -1,44 +1,15 @@
-import React from 'react'
-
-function Contact() {
+import React from 'react';
+const Video = () => {
   return (
-    <div>
+    <iframe 
+	width="100%" 
+	height="555" 
+	src="https://www.youtube.com/embed/u-j9PW2syHE" 
+	title="YouTube video player" 
+	frameborder="0" 
+	allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+	allowfullscreen></iframe>
+  );
+};
 
-      <div class="card-group">
-        <div class="card">
-          <img alt="Qries" src="https://wonderfulengineering.com/wp-content/uploads/2014/10/wallpaper-photos-31.jpg" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-        <div class="card">
-          <img alt="Qries" src="https://wonderfulengineering.com/wp-content/uploads/2014/10/wallpaper-photos-31.jpg" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-        <div class="card">
-          <img alt="Qries" src="https://wonderfulengineering.com/wp-content/uploads/2014/10/wallpaper-photos-31.jpg" />
-          <div class="card-body">
-            <h5 class="card-title">Card title</h5>
-            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">Last updated 3 mins ago</small>
-          </div>
-        </div>
-      </div>
-
-    </div>
-  )
-}
-
-export default Contact
+export default Video;
